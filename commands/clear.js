77b3564f72d8eab09f69129bd7ec05b440e343cc
TomaszBachorski <tomekbachorski@o2.pl>
2022-07-23 12:49:10 +0100

    clear command

---
+++ commands/clear.js
@@ -0,0 +1,23 @@
+const Discord = require("discord.js");
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const settings = require("../settings.json");
+const { PermissionFlagsBits } = require("discord-api-types/v10");
+module.exports = {
+    name: "clear",
+    aliases: [],
+    description: "Czyszczenie czatu",
+    usage: "ilośc wiadomości",
+    data:  function() {return new SlashCommandBuilder()
+        .setName(this.name)
+        .setDescription(this.description)
+        .addIntegerOption(option =>
+            option.setName("ilość").setDescription("Ilość wiadomości").setRequired(true).setMinValue(1).setMaxValue(50))
+        .setDefaultMemberPermissions(PermissionFlagsBits.ManageMessages)
+    },
+    execute: async (interaction, client)=> {
+        let number = interaction.options.getInteger("ilość");
+        client.channels.cache.get(interaction.channelId).bulkDelete(number);
+        interaction.reply({content: `Usunięto ${number} wiadomości`, ephemeral: true});
+        return;
+    },
+}
